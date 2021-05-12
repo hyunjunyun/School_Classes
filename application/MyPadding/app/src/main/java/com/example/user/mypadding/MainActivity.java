a19e04f0994f20b64c35f296950b336811baa552
hyunjun yun <kkkths@naver.com>
2021-05-12 11:57:19 +0900

    create
    school class

---
+++ application/MyPadding/app/src/main/java/com/example/user/mypadding/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.user.mypadding;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public Button createBtn() {
+//        View view =
+//        button.setText("버튼입니다");
+
+//        ViewGroup.LayoutParams lp = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        button.setLayoutParams(lp);
+//
+//        button.setOnClickListener({
+//                Toast.makeText(this, "버튼입니다.", Toast.LENGTH_SHORT).show()
+//        });
+
+        ;
+        return null;
+    }
+}
