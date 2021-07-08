84fba0f15d6983bb8a2fad62266d4beb65010632
hyunjun yun <kkkths@naver.com>
2021-07-08 16:06:31 +0900

    20210708

---
+++ application/ImageView/app/src/main/java/com/example/user/imageview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.imageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
